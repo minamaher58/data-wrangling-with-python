8cd3076d5400063ed6c5b30f22b3f8728f3a1ffb
minamaher58 <minamahertalaat58@gmail.com>
2023-11-22 07:25:17 +0200

    Crawling Whole Websites with Scrapy

--- ch12 - Advanced Web Scraping Screen Scrapers and Spiders/emojispider/emojispider/items.py
+++ ch12 - Advanced Web Scraping Screen Scrapers and Spiders/emojispider/emojispider/items.py
@@ -10,3 +10,13 @@ class EmojispiderItem(scrapy.Item):
     emoji_handle = scrapy.Field()
     emoji_image = scrapy.Field()
     section = scrapy.Field()
+
+class PythonPackageItem(scrapy.Item):
+    package_name = scrapy.Field()
+    version_number = scrapy.Field()
+    package_downloads = scrapy.Field()
+    package_page = scrapy.Field()
+    package_short_description = scrapy.Field()
+    home_page = scrapy.Field()
+    python_versions = scrapy.Field()
+    last_month_downloads = scrapy.Field()
